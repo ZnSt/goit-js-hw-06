73742d05b1a053fc7d3604f823adb2d1ed1eeb22
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-03 16:23:11 +0200

    Сделал первые 9 д/з

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+  btn: document.querySelector('button[type="submit"]'),
+};
 
+function onSubmit(event) {
+  event.preventDefault();
+  const inputEmail = event.target.elements.email;
+  const inputPassword = event.target.elements.password;
+
+  if (inputEmail.value === "" || inputPassword.value === "") {
+    alert("Заполните, пожалуйста, пустые поля!");
+  } else {
+    const obj = {
+      email: inputEmail.value,
+      password: inputPassword.value,
+    };
+    console.log(obj);
+    refs.form.reset();
+  }
+}
+
+refs.form.addEventListener("submit", onSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+console.log(getRandomHexColor());
+
+const refs = {
+  span: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+  body: document.body,
+};
+
+function handleBtn() {
+  const randomColor = getRandomHexColor();
+  refs.span.textContent = randomColor;
+  refs.body.style.backgroundColor = randomColor;
+}
+
+refs.btn.addEventListener("click", handleBtn);
